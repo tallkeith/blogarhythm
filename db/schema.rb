6bee51ada9f41843f9f8ae5badacb03705da5bf9
Philip <pcharlem01@gmail.com>
2014-06-02 10:45:06 -0400

    identity model added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602130650) do
+ActiveRecord::Schema.define(version: 20140602144426) do
+
+  create_table "identities", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "providers", force: true do |t|
     t.string   "uid"
